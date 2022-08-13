ff4febce098be97e3e6e9dce9c5710d882e8b432
Bruno Sonnino <sonnino@revolution.com.br>
2022-08-13 18:16:39 -0400

    Original Commit

---
+++ ConvertersInUWP/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ConvertersInUWP.ViewModel;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ConvertersInUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MainViewModel _mainVm;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _mainVm = App.Current.MainVM;
+        }
+    }
+}
---
+++ ConvertersInUWP/ViewModel/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CustomerLib;
+
+namespace ConvertersInUWP.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private IEnumerable<Customer> _allCustomers;
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(RemoveCommand))]
+        private Customer _selectedCustomer;
+        [ObservableProperty]
+        private IEnumerable<Customer> _customers;
+        [ObservableProperty]
+        private string _searchText;
+
+        public MainViewModel(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository ??
+                                  throw new ArgumentNullException("customerRepository");
+            GetCustomers();
+        }
+
+        private async void GetCustomers()
+        {
+            _allCustomers = await _customerRepository.GetCustomersAsync();
+            Search();
+        }
+
+
+        [RelayCommand]
+        private void Add()
+        {
+            var customer = new Customer();
+            _customerRepository.Add(customer);
+            SelectedCustomer = customer;
+            OnPropertyChanged("Customers");
+        }
+
+        [RelayCommand(CanExecute = "HasSelectedCustomer")]
+        private void Remove()
+        {
+            if (SelectedCustomer != null)
+            {
+                _customerRepository.Remove(SelectedCustomer);
+                SelectedCustomer = null;
+                OnPropertyChanged("Customers");
+            }
+        }
+
+        private bool HasSelectedCustomer() => SelectedCustomer != null;
+
+        [RelayCommand]
+        private void Save()
+        {
+            _customerRepository.Commit();
+        }
+
+        [RelayCommand]
+        public void Search()
+        {
+            if (!string.IsNullOrWhiteSpace(SearchText))
+                Customers = _allCustomers.Where(c => ((Customer)c).Country.ToLower().Contains(SearchText.ToLower()));
+            else
+                Customers = _allCustomers;
+            OnPropertyChanged("Customers");
+        }
+    }
+}
